059fe6cd3ad2d3fcbba6e7f0ff04d8c723f0cc80
doramatadora <hello@doramilitaru.com>
2020-04-21 10:00:31 +0100

    add farmers population

--- assets/client.js
+++ assets/client.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
-    const farmers = document.querySelector("#farmers > tbody")
-    getParties().then(parties => {
-        console.log({ parties, farmers })
+  const farmers = $("#farmers > tbody");
 
-    })
+  getParties().then((parties) => {
+    parties.forEach((p) => {
+      farmers.append(`
+            <tr class="table-row table-row-default">
+                <td class="name">${p.Name}</td>
+                <td class="type">${p.Type}</td>
+                <td class="location">${p.Country}</td>
+                <td class="remove">
+                    <div class="remove-img"></div>
+                </td>
+            </tr>`);
+    });
+  });
 });
